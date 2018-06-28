16122b3fccf8d4092b80757f2822d5e2fa419b8c
nandhoz <nandhoz@hotmail.com>
2018-06-28 16:30:18 -0500

    prueba

--- resources/views/theme/layout.blade.php
+++ resources/views/theme/layout.blade.php
@@ -639,50 +639,31 @@
         <h1 id="clients" class="title text-center">Clientes</h1>
         <div class="space"></div>
         <div class="row">
-            <!--
+
             <div class="col-md-4">
                 <div class="media testimonial">
                     <div class="media-left">
-                        <img src="{{ asset('theme/images/testimonial-1.png') }}" alt="">
+                        <img src="{{ asset('theme/images/sofi.png') }}" width="300" height="150" alt="">
                     </div>
-                    <div class="media-body">
-                        <h3 class="media-heading">You are Amazing!</h3>
-                        <blockquote>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure aperiam consequatur quo.</p>
-                            <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
-                        </blockquote>
+
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="media testimonial">
+                    <div class="media-left">
+                        <img src="{{ asset('theme/images/R&A.JPG') }}"  width="300" height="150" alt="">
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="media testimonial">
                     <div class="media-left">
-                        <img src="{{ asset('theme/images/testimonial-2.png') }}" alt="">
-                    </div>
-                    <div class="media-body">
-                        <h3 class="media-heading">Yeah!</h3>
-                        <blockquote>
-                            <p>Iure aperiam consequatur quo quis exercitationem reprehenderit dolor vel ducimus.</p>
-                            <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
-                        </blockquote>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="media testimonial">
-                    <div class="media-left">
-                        <img src="{{ asset('theme/images/testimonial-3.png') }}" alt="">
-                    </div>
-                    <div class="media-body">
-                        <h3 class="media-heading">Thank You!</h3>
-                        <blockquote>
-                            <p>Aperiam consequatur quo quis exercitationem reprehenderit suscipit iste placeat.</p>
-                            <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
-                        </blockquote>
+                        <img src="{{ asset('theme/images/Contraconjur.png') }}"  width="300" height="150" alt="">
                     </div>
                 </div>
             </div>
         </div>
+    <!--
         <div class="row">
             <div class="col-md-4">
                 <div class="media testimonial">
@@ -690,8 +671,8 @@
                         <img src="{{ asset('theme/images/testimonial-2.png') }}" alt="">
                     </div>
                     <div class="media-body">
-                        <h3 class="media-heading">Thank You!</h3>
-                        <blockquote>
+                        <h3 class="media-heading">Solfinanzas</h3>
+                       <blockquote>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure aperiam consequatur quo.</p>
                             <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
                         </blockquote>
@@ -705,10 +686,6 @@
                     </div>
                     <div class="media-body">
                         <h3 class="media-heading">Amazing!</h3>
-                        <blockquote>
-                            <p>Iure aperiam consequatur quo quis exercitationem reprehenderit dolor vel ducimus.</p>
-                            <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
-                        </blockquote>
                     </div>
                 </div>
             </div>
@@ -719,16 +696,12 @@
                     </div>
                     <div class="media-body">
                         <h3 class="media-heading">Best!</h3>
-                        <blockquote>
-                            <p>Aperiam consequatur quo quis exercitationem reprehenderit suscipit iste placeat.</p>
-                            <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
-                        </blockquote>
                     </div>
                 </div>
             </div>
-         -->
         </div>
     </div>
+    -->
     <!-- section start -->
     <!-- ================ -->
     <div class="translucent-bg blue">
